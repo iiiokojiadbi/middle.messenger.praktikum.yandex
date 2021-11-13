e2b8781871e449f0f197fcd19af60ec109fcc63d
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2021-11-13 18:20:50 +0300

    добавил postcss

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('test')
+import styles from './styles.css';
+
+const root = document.querySelector('#root');
+
+console.log(styles.test, styles);
+
+const div = document.createElement('div');
+div.textContent = 'test';
+div.classList.add(styles.test);
+
+root.appendChild(div);
