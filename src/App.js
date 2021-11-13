f2db127d60d90a0558d1ded3dfbd842aeb85e4cd
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2021-11-13 22:15:39 +0300

    проверка порта, удалил комменты

--- src/App.js
+++ src/App.js
@@ -3,36 +3,23 @@ import SignupPage from './pages/Signup';
 
 const root = document.querySelector('#root');
 
-const signPage = SigninPage.compile();
-const signupPage = SignupPage.compile();
-
 window.onload = function () {
   if (window.location.pathname === '/') {
     window.location.pathname = '/signin';
   }
 
   if (window.location.pathname === '/signin') {
+    const signPage = SigninPage.compile();
+
     root.innerHTML = signPage;
   }
 
   if (window.location.pathname === '/signup') {
+    const signupPage = SignupPage.compile();
+
     root.innerHTML = signupPage;
   }
 };
-
-// console.log(window.location);
-
-// if (window.location.pathname === '/signin') {
-// }
-
-// if (window.location.pathname === '/') {
-//   const a = document.createElement('a');
-//   a.textContent = 'app';
-
-//   a.setAttribute('href', '/signin');
-
-//   root.appendChild(a);
-// }
 
 if (module.hot) {
   module.hot.accept();
