e1637637efef2619c41451b6b8c6c35b90275bbd
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2022-01-04 18:56:20 +0300

    sprint_2: ожидаю построения ДОМ-дерева

--- src/App.js
+++ src/App.js
@@ -15,35 +15,37 @@ import ProfilePage from './pages/Profile';
 import SignPage from './pages/Sign';
 import Router from './utils/Router';
 
-const root = document.querySelector('#root');
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.querySelector('#root');
 
-const chatPage = new ChatPage({ root, configs: {} });
-const signinPage = new SignPage({ root, configs: signinConfig });
-const signupPage = new SignPage({ root, configs: signupConfig });
-const errorPage = new ErrorPage({ root, configs: notFoundConfig });
-const notFoundPage = new ErrorPage({ root, configs: internalErrorConfig });
-const profilePage = new ProfilePage({
-  root,
-  configs: {
-    [ROUTE.PROFILE]: profileConfig,
-    [ROUTE.PROFILE_EDIT]: profileEditInfoConfig,
-    [ROUTE.PROFILE_CHANGE_PASSWORD]: profileChangePasswordConfig,
-  },
+  const chatPage = new ChatPage({ root, configs: {} });
+  const signinPage = new SignPage({ root, configs: signinConfig });
+  const signupPage = new SignPage({ root, configs: signupConfig });
+  const errorPage = new ErrorPage({ root, configs: notFoundConfig });
+  const notFoundPage = new ErrorPage({ root, configs: internalErrorConfig });
+  const profilePage = new ProfilePage({
+    root,
+    configs: {
+      [ROUTE.PROFILE]: profileConfig,
+      [ROUTE.PROFILE_EDIT]: profileEditInfoConfig,
+      [ROUTE.PROFILE_CHANGE_PASSWORD]: profileChangePasswordConfig,
+    },
+  });
+
+  const pages = {
+    [ROUTE.CHAT]: chatPage,
+    [ROUTE.SIGN_IN]: signinPage,
+    [ROUTE.SIGN_UP]: signupPage,
+    [ROUTE.NOT_FOUND]: errorPage,
+    [ROUTE.ERROR]: notFoundPage,
+    [ROUTE.PROFILE]: profilePage,
+    [ROUTE.PROFILE_EDIT]: profilePage,
+  };
+
+  const router = new Router(pages);
+
+  router.redirect(pathname, null, pathname);
 });
-
-const pages = {
-  [ROUTE.CHAT]: chatPage,
-  [ROUTE.SIGN_IN]: signinPage,
-  [ROUTE.SIGN_UP]: signupPage,
-  [ROUTE.NOT_FOUND]: errorPage,
-  [ROUTE.ERROR]: notFoundPage,
-  [ROUTE.PROFILE]: profilePage,
-  [ROUTE.PROFILE_EDIT]: profilePage,
-};
-
-const router = new Router(pages);
-
-router.redirect(pathname, null, pathname);
 
 if (module.hot) {
   module.hot.accept();
